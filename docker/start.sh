9039e9b7fe9e6fc7447c117c9e7313415e710030
jjehl <julien.jehl@roboticia.com>
2023-08-15 10:46:30 +0200

    rename project to more generic.

--- docker/start.sh
+++ docker/start.sh
@@ -2,4 +2,4 @@
 python3 /App/Api_IA/manage.py migrate > /App/log_migrate-$(date +"%Y_%m_%d_%T").log
 python3 /App/Api_IA/manage.py collectstatic --noinput > /App/log_collect_static-$(date +"%Y_%m_%d_%T").log
 env >> /etc/environment
-cd Api_iA && /App/Api_IA/asgi.sh
+cd Api_IA && /App/Api_IA/asgi.sh
