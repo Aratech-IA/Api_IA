fde171333c2941f0fce8598e6c74bd402cddedb4
jjehl <julien.jehl@aratech.fr>
2023-08-15 09:55:33 +0200

    rename project to more generic.

--- app1_objects_recognition/apps.py
+++ app1_objects_recognition/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class App1AnalyseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app1_analyse'
+    name = 'app1_objects_recognition'
